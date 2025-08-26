184ac71550a7c353bc15c7142ad5ddf0aac2b0c0
Lucas Vasconcellos <lucasv.profissional@gmail.com>
2025-08-26 11:15:37 -0300

    Initial commit

---
+++ src/main/java/com/app/model/Tasks.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.app.model;
+
+/**
+ *
+ * @author lucas.cortes
+ */
+public class Tasks {
+    
+}
---
+++ src/main/java/com/app/view/PrincipalView.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.app.view;
+
+/**
+ *
+ * @author lucas.cortes
+ */
+public class PrincipalView {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
